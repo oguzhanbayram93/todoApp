4e9dd014ff33288375636011a3864d87f6cd8dc4
oguzhanbayram93 <oguzhan_1907_bayram@hotmail.com>
2021-10-11 02:00:34 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState} from 'react';
+import { StyleSheet, Text, View ,KeyboardAvoidingView, TextInput, TouchableOpacity, Keyboard} from 'react-native';
+import Task from './Task';
 
 export default function App() {
+  const [list, setList] = useState("");
+  const [task, setTask] = useState([]);
+  const handleText = () => {
+    Keyboard.dismiss();
+    setTask([...task,list]);
+    setList(null);
+  }
+
+  const deleteTask =(index)=> {
+    let deletecopy = [...task];
+    deletecopy.splice(index, 1);
+    setTask(deletecopy);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inner_container}>
+      <Text style={styles.header }>today's works</Text>
+        {
+          task.map((item, i) => {
+            return (
+              <TouchableOpacity onPress={deleteTask }>
+
+              <Task key={i} item={item} />
+              </TouchableOpacity>
+            );
+          
+          })
+        }
+      
+     
+      </View>
+      
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
+        <View  style={styles.touchContainer}>
+          <TextInput style={styles.input} placeholder="yapılacak..." value={list} onChangeText={(text)=>{return setList(text)} }/>
+          <TouchableOpacity onPress={handleText}>
+            <View style={styles.button }>
+            <Text>+</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+
     </View>
   );
 }
@@ -14,8 +54,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
+    flexDirection: "column",
+    backgroundColor: "#868e96",
+   justifyContent: "space-between",
   },
+  header: {
+    fontWeight:"bold",
+    fontSize: 24,
+    marginTop: 50,
+    marginLeft:10
+  },
+  input: {
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    width: 225,
+    backgroundColor: "white",
+  },
+touchContainer: {
+  flexDirection: "row",
+  justifyContent: "space-around",
+  marginBottom: 50,
+  },
+  button: {
+    borderRadius: 50,
+    backgroundColor: "white",
+    justifyContent: "center",
+    padding:20,
+  }
+            
 });
